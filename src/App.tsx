f0856ad03258febdc062c3026e05bfbb28ea0aa9
evgeniy <not-found@inbox.ru>
2023-11-28 23:28:14 +0300

    Add code according to videos 1-6

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className='App'>
+      <h1>Simple HTML</h1>
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
