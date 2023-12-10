975786ab2db0229b46bf9727c838c1b5936e3aea
evgeniy <not-found@inbox.ru>
2023-12-11 00:05:27 +0300

    Add code according to videos 10-11

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
 
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <Technologies />
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://goo.su/wQx3i"
+                    alt="logo"/>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a href="#1">Profile</a>
+                </div>
+                <div>
+                    <a href="#2">Messages</a>
+                </div>
+                <div>
+                    <a href="#3">News</a>
+                </div>
+                <div>
+                    <a href="#4">Music</a>
+                </div>
+                <div>
+                    <a href="#5">Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img src="https://goo.su/d6QT" alt="thumbnail"/>
+                </div>
+                <div>
+                    ava + desc
+                </div>
+                <div>
+                    my posts
+                    <div>
+                        new post
+                    </div>
+                </div>
+                <div>
+                    postlist
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
